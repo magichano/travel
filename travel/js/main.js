47f454f8a12f75e2c75a4cde5e683babd207cd50
magichano <antonartamonov1@gmail.com>
2023-01-26 14:25:53 +0300

    slider abled

--- travel/js/main.js
+++ travel/js/main.js
@@ -92,7 +92,7 @@ register.addEventListener('click', ()=>{
         emailInput.value='';
         passwordInput.value='';
     } else{
-        dontHaveAcc.classList.remove('hidden') ;
+        dontHaveAcc.classList.remove('hidden');
         toRemove.classList.remove('hidden');
         dontHaveAccText.innerHTML="Don’t have an account?";
         register.innerHTML="Register";
@@ -149,14 +149,23 @@ signIn.addEventListener('click', () =>{
 
 let imagesForSlider=document.querySelector('.pdImgContainers');
 let slider=[];
+
+let leftImage=document.querySelector('.pdImgContainer1');
+let centerImage=document.querySelector('.pdImgContainer2');
+let rightImage=document.querySelector('.pdImgContainer3');
+
 let leftPoint=document.querySelector('.leftPoint');
-let centerPoint=document.querySelector('.centerPoint');
 let rightPoint=document.querySelector('.rightPoint');
+let centerPoint=document.querySelector('.centerPoint');
+
 let k=1;
+
+let figures=document.querySelector('.figures');
+/*
 let pdImgContainer1=document.querySelector('.pdImgContainer1');
 let pdImgContainer2=document.querySelector('.pdImgContainer2');
 let pdImgContainer3=document.querySelector('.pdImgContainer3');
-
+*/
 
 
 for (let i = 0; i <= 5; i++) {
@@ -165,57 +174,163 @@ for (let i = 0; i <= 5; i++) {
     }
   }
 
-  //console.log(slider);
+  console.log(k);
 
+
+
+/* PRESS THE IMAGES*/
+  leftImage.addEventListener('click', () =>{
+    if (k==1){
+        figures.classList.toggle('translateLeftImage64');
+
+        leftPoint.classList.toggle('translateRightPoint150');
+        centerPoint.classList.toggle('translateLeftPoint150');
+
+        k=0;
+    } else if (k==2){
+        figures.classList.toggle('translateLeftImage64');
+        figures.classList.remove('translateRightImage64');
+
+       
+
+        k=0;
+    }
+})
+
+    centerImage.addEventListener('click', () =>{
+        figures.classList.remove('translateLeftImage64');
+        figures.classList.remove('translateRightImage64');
+        
+        if (k==0){
+            leftPoint.classList.toggle('translateRightPoint150');
+            centerPoint.classList.toggle('translateLeftPoint150');
+        } else if (k==2){
+            rightPoint.classList.toggle('translateLeftPoint150');
+            centerPoint.classList.toggle('translateRightPoint150');
+        }
+       
+        k=1;
+    }
+)
+
+    rightImage.addEventListener('click', () =>{
+    if (k==0){
+        figures.classList.toggle('translateRightImage64');
+        figures.classList.remove('translateLeftImage64');
+        k=2;
+    } else if (k==1){
+        figures.classList.toggle('translateRightImage64');
+
+        rightPoint.classList.toggle('translateLeftPoint150');
+        centerPoint.classList.toggle('translateRightPoint150');
+
+        k=2;
+    }
+})
+/* PRESS THE IMAGES*/
+
+
+/* PRESS THE POINTS*/
+/* 
   leftPoint.addEventListener('click', () =>{
     if (k==1){
-        pdImgContainer1.classList.add('translateLeftPoint64');
-        pdImgContainer2.classList.add('translateLeftPoint64');
-        pdImgContainer3.classList.add('translateLeftPoint64');
-
-        pdImgContainer1.classList.remove('translateLeftPoint64');
-        pdImgContainer2.classList.remove('translateLeftPoint64');
-        pdImgContainer3.classList.remove('translateLeftPoint64');
-
-        pdImgContainer1.classList.remove('translateLeftPoint64');
-        pdImgContainer2.classList.remove('translateLeftPoint64');
-        pdImgContainer3.classList.remove('translateLeftPoint64');
-
-
+        figures.classList.toggle('translateLeftImage64');
         k=0;
     } else if (k==2){
-        pdImgContainer1.classList.add('translateLeftPoint128');
-        pdImgContainer2.classList.add('translateLeftPoint128');
-        pdImgContainer3.classList.add('translateLeftPoint128');
+        figures.classList.toggle('translateLeftImage64');
+        figures.classList.remove('translateRightImage64');
         k=0;
     }
 })
+
+    centerPoint.addEventListener('click', () =>{
+        figures.classList.remove('translateLeftImage64');
+        figures.classList.remove('translateRightImage64');
+        k=1;
+    }
+)
 
     rightPoint.addEventListener('click', () =>{
-        if (k==0){
-            pdImgContainer1.classList.toggle('translateRightPoint128');
-            pdImgContainer2.classList.toggle('translateRightPoint128');
-            pdImgContainer3.classList.toggle('translateRightPoint128');
-            k=2;
-        } else if (k==1){
-            pdImgContainer1.classList.toggle('translateRightPoint64');
-            pdImgContainer3.classList.toggle('translateRightPoint64');
-            pdImgContainer2.classList.toggle('translateRightPoint64');
-        }
-})
-
-centerPoint.addEventListener('click', () =>{
-    if (k==1){
-        pdImgContainer1.classList.add('translateLeftPoint64');
-        pdImgContainer2.classList.add('translateLeftPoint64');
-        pdImgContainer3.classList.add('translateLeftPoint64');
-        k=0;
-    } else if (k==2){
-        pdImgContainer1.classList.add('translateLeftPoint128');
-        pdImgContainer2.classList.add('translateLeftPoint128');
-        pdImgContainer3.classList.add('translateLeftPoint128');
+    if (k==0){
+        figures.classList.toggle('translateRightImage64');
+        figures.classList.remove('translateLeftImage64');
+        k=2;
+    } else if (k==1){
+        figures.classList.toggle('translateRightImage64');
+        k=2;
     }
 })
+*/
+/* PRESS THE POINTS*/
+
+
+
+/* PRESS THE ARROWS*/
+
+let l=0;
+let leftArrow=document.querySelector('.arrowSelectLeft');
+let rightArrow=document.querySelector('.arrowSelectRight');
+
+let leftArrowBright=document.querySelector('.arrowSelectLeftBright');
+let rightArrowBright=document.querySelector('.arrowSelectRightBright');
+
+rightArrowBright.addEventListener('click', () =>{
+    if (l==0){
+        figures.classList.toggle('translateRightImage375');
+
+        leftArrow.classList.toggle('setArrowUnactive');
+        leftArrowBright.classList.toggle('setArrowActive');
+
+        leftPoint.classList.toggle('translateRightPoint0');
+        centerPoint.classList.toggle('translateLeftPoint0');
+
+        l=1;
+    } else if (l==1) {
+        figures.classList.toggle('translateRightImage375');
+        figures.classList.toggle('translateRightImage750');
+
+        rightArrow.classList.toggle('setArrowActive');
+        rightArrowBright.classList.toggle('setArrowUnactive');
+
+        rightPoint.classList.toggle('translateRightPoint177');
+        centerPoint.classList.toggle('translateLeftPoint177');
+        
+        l=2;
+    }
+
+})
+
+leftArrowBright.addEventListener('click', () =>{
+    if (l==2){
+        figures.classList.toggle('translateRightImage375');
+        figures.classList.toggle('translateRightImage750');
+
+        rightArrow.classList.toggle('setArrowActive');
+        rightArrowBright.classList.toggle('setArrowUnactive');
+
+        rightPoint.classList.toggle('translateRightPoint177');
+        centerPoint.classList.toggle('translateLeftPoint177');
+
+        l=1;
+
+    } else if (l==1) {
+        figures.classList.toggle('translateRightImage375');
+
+        leftArrow.classList.toggle('setArrowUnactive');
+        leftArrowBright.classList.toggle('setArrowActive');
+
+        leftPoint.classList.toggle('translateRightPoint0');
+        centerPoint.classList.toggle('translateLeftPoint0');
+        
+        l=0;
+    }
+
+})
+
+
+/* PRESS THE ARROWS*/
+
+
 
 /*
 for (let node of imagesForSlider.children){
